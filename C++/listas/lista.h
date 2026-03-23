2a903cf540d83956eda421410d83ee56f48c1b5c
Igor-S4ntiago <igorsantiagopf@gmail.com>
2026-03-23 09:33:19 -0300

    feature:lista em C

---
+++ C++/listas/lista.h
@@ -0,0 +1,12 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+// configurações gerais
+
+#define TAM_TITULO 96
+#define TAM_NOME 48
+
+// definições de status
+
+#define A_FAZER 0 
+#define EM-
---
+++ C++/listas/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lista.h"
+
+int main () {
+    Lista * MinhaLista = CriarLista(20);
+}
